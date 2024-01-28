afa867b9da0f4798bf00843e9758a364b9e33522
Katrin Boberg Bevemyr <katrin@bevemyr.com>
2024-01-28 15:59:24 +0100

    Initial Commit

---
+++ CurlingTimer Watch App/CurlingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurlingTimerApp.swift
+//  CurlingTimer Watch App
+//
+//  Created by Katrin Boberg Bevemyr on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct CurlingTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurlingTimer/CurlingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurlingTimerApp.swift
+//  CurlingTimer
+//
+//  Created by Katrin Boberg Bevemyr on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct CurlingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
