f1ad4f89d1d8981449d8df0d1404c6a267638723
Katrin Boberg Bevemyr <katrin@bevemyr.com>
2024-10-13 21:11:39 +0200

    Nytt färgschema för att matcha logotypen.

---
+++ CurlingTimer Watch App/LogHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  LogHeaderView.swift
+//  CurlingTimer Watch App
+//
+//  Created by Katrin Boberg Bevemyr on 2024-10-12.
+//
+
+import SwiftUI
+
+struct LogHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogHeaderView()
+}
